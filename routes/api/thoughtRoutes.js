f29193bc087278610216c29a3aabae0fd00c8bdb
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-07-26 13:45:50 -0500

    thoughtAndUserRouti

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+
+module.exports = router;
